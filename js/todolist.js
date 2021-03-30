3b350ff126e8a7da42fb28d8a85bf059def51418
aimee0718 <aimee_li0718@163.com>
2021-03-30 13:02:32 +0800

    add edit function

--- js/todolist.js
+++ js/todolist.js
@@ -18,25 +18,40 @@ $(function () {
 
     });
 
-    $('.doing ol').on('click', 'li', function () {
+    $('ol').on('dblclick', 'li', function () {
         var todolists = readData();
-        var index = $(this).attr('data-index');
-        todolists[index].done = true;
+        window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+        var index = $(this).children('em').attr('id');
+        $(this).html('<input type="checkbox"/><input type="text" value="' + todolists[index].title + '"/><em id=' + index + '>删除</em>');
+        $(this).children('input[type="text"]').select();
+        $(this).children('input[type="text"]').on('blur', function () {
+            todolists[index].title = $(this).val();
+            writeData(todolists);
+            loadData();
+            getDoingSum();
+            getDoneSum();
+        });
+        $(this).children('input[type="text"]').on('keydown', function (e) {
+            if (e.keyCode == 13) {
+                todolists[index].title = $(this).val();
+                writeData(todolists);
+                loadData();
+                getDoingSum();
+                getDoneSum();
+            }
+        })
+    })
+
+    $('ol').on('click', 'input', function () {
+        var todolists = readData();
+        var index = $(this).siblings('em').attr('id');
+        todolists[index].done = $(this).prop("checked");
         writeData(todolists);
-        loadData()
+        loadData();
         getDoingSum();
         getDoneSum();
     })
 
-    $('.done ol').on('click', 'li', function () {
-        var todolists = readData();
-        var index = $(this).attr('data-index');
-        todolists[index].done = false;
-        writeData(todolists);
-        loadData()
-        getDoingSum();
-        getDoneSum();
-    })
 
     $('ol').on("mouseenter", "em", function () {
         $(this).css("background-color", "steelblue");
@@ -46,7 +61,7 @@ $(function () {
     });
     $('ol').on('click', 'em', function () {
         var todolists = readData();
-        var index = $(this).parent().attr('data-index');
+        var index = $(this).attr('id');
         todolists.splice(index, 1);
         writeData(todolists);
         loadData();
@@ -82,13 +97,12 @@ $(function () {
         data = JSON.parse(data);
         $('ol').empty();
         $.each(data, function (i, ele) {
-            var li = $('<li' + ' data-index=' + i + '></li>');
-
+            var li = $('<li></li>');
             if (ele.done == false) {
-                li.html('<input type="checkbox">' + ele.title + '<em></em>');
+                li.html('<input type="checkbox"><span>' + ele.title + '</span><em id=' + i + '>删除</em>');
                 $('.doing ol').prepend(li);
             } else {
-                li.html('<input type="checkbox" checked="checked">' + ele.title + '<em></em>');
+                li.html('<input type="checkbox" checked="checked"><span>' + ele.title + '</span><em id=' + i + '>删除</em>');
                 $('.done ol').prepend(li);
             }
         })
